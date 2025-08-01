0917103c4e833477dcbc575baa315320aa872920
Vundekoti Vidyasagar <97279970+VIDYASAGARVV@users.noreply.github.com>
2025-08-01 13:15:31 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,13 +14,35 @@ function Projects() {
       <div className="container">
         <h2 className="mb-4">Projects</h2>
         <div className="row">
-          <div className="col-md-4 mb-3">
+          <div className="col-md-3 mb-3">
             <Link
               to="/projects/1"
               className="text-decoration-none text-dark"
               onClick={() => handleCardClick(1)}
             >
-              <div className={`card h-100 ${activeCard === 1 ? 'active-card' : ''}`}>
+              <div className={`card h-100 ${activeCard === 2 ? 'active-card' : ''}`}>
+                <div className="card-body">
+                  <h5 className="card-title">
+                    Reactjs Frontend
+                    <img
+                      src="https://www.clipartmax.com/png/middle/8-84311_arrow-copy-and-paste.png"
+                      alt="arrow"
+                      style={{ height: '20px' }}
+                      className="round"
+                    />
+                  </h5>
+                  <p className="card-text">Description of project one.</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="col-md-3 mb-3">
+            <Link
+              to="/projects/2"
+              className="text-decoration-none text-dark"
+              onClick={() => handleCardClick(1)}
+            >
+              <div className={`card h-100 ${activeCard === 2 ? 'active-card' : ''}`}>
                 <div className="card-body">
                   <h5 className="card-title">
                     Dynamic Websites{' '}
@@ -37,13 +59,13 @@ function Projects() {
             </Link>
           </div>
 
-          <div className="col-md-4 mb-3">
+          <div className="col-md-3 mb-3">
             <Link
-              to="/projects/2"
+              to="/projects/3"
               className="text-decoration-none text-dark"
               onClick={() => handleCardClick(2)}
             >
-              <div className={`card h-100 ${activeCard === 2 ? 'active-card' : ''}`}>
+              <div className={`card h-100 ${activeCard === 3 ? 'active-card' : ''}`}>
                 <div className="card-body">
                   <h5 className="card-title">CRM Software</h5>
                   <p className="card-text">Description of project two.</p>
@@ -51,13 +73,13 @@ function Projects() {
               </div>
             </Link>
           </div>
-          <div className="col-md-4 mb-3">
+          <div className="col-md-3 mb-3">
             <Link
-              to="/projects/3"
+              to="/projects/4"
               className="text-decoration-none text-dark"
               onClick={() => handleCardClick(3)}
             >
-              <div className={`card h-100 ${activeCard === 3 ? 'active-card' : ''}`}>
+              <div className={`card h-100 ${activeCard === 4 ? 'active-card' : ''}`}>
                 <div className="card-body">
                   <h5 className="card-title">NXT Wave Internship</h5>
                   <p className="card-text">Description of project three.</p>
@@ -67,6 +89,7 @@ function Projects() {
           </div>
         </div>
       </div>
+      <div className='h-50'></div>
     </section>
   );
 }
